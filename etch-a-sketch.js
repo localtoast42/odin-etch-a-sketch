6fcf427aa026aa7e295340177c9611c1fde6d50d
localtoast42 <81132724+localtoast42@users.noreply.github.com>
2024-01-26 12:40:30 -0500

    Add CSS classes to grid divs

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -3,9 +3,11 @@ const container = document.querySelector('.container');
 for (let i = 0; i < 4; i++) {
     const row_div = document.createElement('div');
     row_div.setAttribute('id', 'row' + i); 
+    row_div.classList.add('grid-row');
     for (let j = 0; j < 4; j++) {
         const col_div = document.createElement('div');
         col_div.setAttribute('id', 'row' + i + 'col' + j); 
+        col_div.classList.add('grid-item');
         row_div.appendChild(col_div);
     }
     container.appendChild(row_div);
